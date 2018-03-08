1f37a8134184af8eda6bccff008e531e944a3d76
Bhagwati <bhagwati@flightsbank.com>
2018-03-09 00:46:17 +0530

    Initial Bare repository.

---
+++ Core/Application.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bare\Core;
+
+class Application {
+    public function run() {
+        echo "<h1>Package loaded correctly</h1><p>Start Editing Bare/Core/Application.php for further package development.</p>";
+    }
+}
